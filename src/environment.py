721a3b0359cf46a797809e4af35a3fd10aa063b9
basic-bgnr <devkotabidit@gmail.com>
2020-07-12 01:05:03 +0545

    changed environment.py to be able to handle multiple scope of variable

--- src/environment.py
+++ src/environment.py
@@ -1,15 +1,25 @@
 class Environment:
-	def __init__(self):
+	def __init__(self, parent=None):
 		self.hashmap = {}
+		self.parent = parent # this is for lexical scoping of variable
 
 	def put(self, lvalue, rvalue):
 		self.hashmap[lvalue] = rvalue
 
 	def get(self, lvalue):
-		return self.hashmap[lvalue]
+		try:
+			ret_val = self.hashmap[lvalue]
+			return ret_val
+		except KeyError: # if not found in current environment search the parent environment for the needed variable
+			if (self.parent != None):
+				return self.parent.get(lvalue)
+			else: # raise exception if the global environment doesn't have the needed variable
+				raise Exception(f'undefined variable {lvalue}')
 
 	def putIfExists(self, lvalue, rvalue):
-		if lvalue in self.hashmap.keys():
-			self.put(lvalue, rvalue)
+		if (lvalue in self.hashmap.keys()):
+			self.put(lvalue, rvalue) # put the value in current scope 
+		elif (self.parent != None):
+			self.parent.putIfExists(lvalue, rvalue) # if the value is not found in current scope put it recursively in parent scope
 		else:
-			raise Exception(f"undefined variable '{lvalue}'")
+			raise Exception(f"undefined variable '{lvalue}'")# raise exception if the global scope doesn't contain the variable
