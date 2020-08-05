37bb48a66cc441fe7767ce6529a8d43b640d4f44
basic-bgnr <devkotabidit@gmail.com>
2020-08-05 19:44:43 +0545

    added code for reading and writing file

--- src/plox.py
+++ src/plox.py
@@ -1,109 +1,115 @@
 import sys
 from lexer import Scanner
-from parser import Parser, Calculator, StatementExecutor, NativeTimer, Exit, Str, Random, Array, InsertAt, DeleteAt, At, Len, Push, Pop
+from parser import Parser, Calculator, StatementExecutor, NativeTimer, Exit, Str, Random, Array, InsertAt, DeleteAt, At, Len, Push, Pop, ReadFile, WriteFile
 from ASTPrinter import ASTPrinter
 from environment import Environment
 from resolver import Resolver
 class Lox:
-	def __init__(self):
-		self.had_error = False
-		self.environment = Environment()
-		self.resolver = Resolver()
+    def __init__(self):
+        self.had_error = False
+        self.environment = Environment()
+        self.resolver = Resolver()
 
-		self.prompt_signature = '#|>>'
-		### initialize native function####
-		timer = NativeTimer()
-		timer.register('timer', self.environment)
+        self.prompt_signature = '#|>>'
+        ### initialize native function####
+        timer = NativeTimer()
+        timer.register('timer', self.environment)
 
-		ext = Exit()
-		ext.register('exit', self.environment)
+        ext = Exit()
+        ext.register('exit', self.environment)
 
-		string = Str()
-		string.register('str', self.environment) 
+        string = Str()
+        string.register('str', self.environment) 
 
-		random = Random()
-		random.register('random', self.environment)
+        random = Random()
+        random.register('random', self.environment)
 
-		array = Array()
-		array.register('array', self.environment)
+        array = Array()
+        array.register('array', self.environment)
 
-		insertAt = InsertAt()
-		insertAt.register('insertAt', self.environment)
+        insert_at = InsertAt()
+        insert_at.register('insertAt', self.environment)
 
-		deleteAt = DeleteAt()
-		deleteAt.register('deleteAt', self.environment)
+        delete_at = DeleteAt()
+        delete_at.register('deleteAt', self.environment)
 
-		at = At()
-		at.register('at', self.environment)
+        at = At()
+        at.register('at', self.environment)
 
-		len_ = Len()
-		len_.register('len', self.environment)
+        len_ = Len()
+        len_.register('len', self.environment)
 
-		push = Push()
-		push.register('push', self.environment)
+        push = Push()
+        push.register('push', self.environment)
 
-		pop = Pop()
-		pop.register('pop', self.environment)
-		##################################
-	
-	@staticmethod
-	def main():
-		if len(sys.argv) > 2:
-			if (sys.argv[2] == '-i'):
-				Lox.runFileInteractive(sys.argv[1], Lox())
-			else:
-				print('''usage: 
-					plox files [-i]
-					-i: run files in interactive mode''')
-			exit(64) #see open_bsd exit code (just for standardization)
-		elif len(sys.argv) == 2:
-			Lox.runFile(sys.argv[1], Lox())
+        pop = Pop()
+        pop.register('pop', self.environment)
 
-		else:
-			Lox.runPrompt(Lox())
+        read_file = ReadFile()
+        read_file.register('readFile', self.environment)
 
-	@staticmethod
-	def runFileInteractive(path, lox_interpreter):
-		Lox.runFile(path, lox_interpreter)
-		Lox.runPrompt(lox_interpreter)
+        write_file = WriteFile()
+        write_file.register('writeFile', self.environment)
+    
+    @staticmethod
+    def main():
+        if len(sys.argv) > 2:
+            if (sys.argv[2] == '-i'):
+                Lox.runFileInteractive(sys.argv[1], Lox())
+            else:
+                print('''usage: 
+                    plox files [-i]
+                    -i: run files in interactive mode''')
+            exit(64) #see open_bsd exit code (just for standardization)
+        elif len(sys.argv) == 2:
+            Lox.runFile(sys.argv[1], Lox())
 
-	@staticmethod
-	def runFile(path, lox_interpreter):
-		with open(path, mode='r') as content:
-			source_code = content.read()
-			lox_interpreter.run(source_code)
-		### add interpreter in prompt mode after this. if necessary arguments are passed
-	
-	@staticmethod
-	def runPrompt(lox_interpreter):
-		while True:
-			print(lox_interpreter.prompt_signature, end=' ')
-			try:
-				lox_interpreter.run(input())
-			except Exception as e: 
-				print(e)
-	
-	def run(self, source_code):
-		#why this runs, its because of the lox interpreter environment, which remains in existence even after this function ends 
-		scanner = Scanner(source_code)
-		scanner.scanTokens()
-		# print(scanner.toString())
-		parser = Parser(scanner.token_list)
-		parser.parse()
-		#print(Calculator().calculate(parser.AST))
-		self.resolver.resolveAll(parser.AST)
-		
+        else:
+            Lox.runPrompt(Lox())
 
-		interpreter = StatementExecutor(self.environment, self.resolver)
-		for ast in parser.AST:
-			interpreter.execute(ast)
-			# print("AST-> ", ASTPrinter().print(ast))
+    @staticmethod
+    def runFileInteractive(path, lox_interpreter):
+        Lox.runFile(path, lox_interpreter)
+        Lox.runPrompt(lox_interpreter)
 
-	def error(self, line, err_msg):
-		report(line, "", err_msg) 
+    @staticmethod
+    def runFile(path, lox_interpreter):
+        with open(path, mode='r') as content:
+            source_code = content.read()
+            lox_interpreter.run(source_code)
+        ### add interpreter in prompt mode after this. if necessary arguments are passed
+    
+    @staticmethod
+    def runPrompt(lox_interpreter):
+        while True:
+            print(lox_interpreter.prompt_signature, end=' ')
+            try:
+                lox_interpreter.run(input())
+            except Exception as e: 
+                print(e)
+    
+    def run(self, source_code):
+        #why this runs, its because of the lox interpreter environment, which remains in existence even after this function ends 
+        scanner = Scanner(source_code)
+        scanner.scanTokens()
+        # print(scanner.toString())
+        parser = Parser(scanner.token_list)
+        parser.parse()
+        #print(Calculator().calculate(parser.AST))
+        self.resolver.resolveAll(parser.AST)
+        
 
-	def report(self, line, where, err_msg):
-		print(f'[Line {line}] of {where} following errors where reported\n{err_msg}')
+        interpreter = StatementExecutor(self.environment, self.resolver)
+        for ast in parser.AST:
+            # print("AST-> ", ASTPrinter().print(ast))
+            interpreter.execute(ast)
+           
+
+    def error(self, line, err_msg):
+        report(line, "", err_msg) 
+
+    def report(self, line, where, err_msg):
+        print(f'[Line {line}] of {where} following errors where reported\n{err_msg}')
 
 
 
